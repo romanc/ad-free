83688a9324e415f405b7c6e8732c6547b6e6c574
romanc <romanc@users.noreply.github.com>
2021-02-07 13:02:09 +0100

    QR codes working

--- adfree/qrcodes/qrcodes.py
+++ adfree/qrcodes/qrcodes.py
@@ -1,10 +1,30 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, render_template, request, send_file
 )
+from io import BytesIO
+import qrcode
 
 bp = Blueprint('qrcodes', __name__, url_prefix='/qr-codes')
 
 
-@bp.route("/")
+@bp.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        content = request.form['content']
+        error = 'Content is required.' if not content else None
+
+        if error is None:
+            # todo - generate qr code
+            # print("qr code content: %s" % content)
+            img = qrcode.make(content)
+            img_io = BytesIO()
+            img.save(img_io, 'png')
+            img_io.seek(0)
+            return send_file(img_io,
+                             attachment_filename="qrcode.png",
+                             as_attachment=True)
+
+        # Flash error message
+        flash(error)
+
     return render_template('qr-codes/index.html')
--- adfree/youtubedl/youtubedl.py
+++ adfree/youtubedl/youtubedl.py
@@ -5,6 +5,16 @@ from flask import (
 bp = Blueprint('youtubedl', __name__, url_prefix='/youtube-dl')
 
 
-@bp.route("/")
+@bp.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        link = request.form['link']
+        error = 'Link is required.' if not link else None
+
+        if error is None:
+            # todo - do something here
+            print("Link: %s" % link)
+        else:
+            flash(error)
+
     return render_template('youtube-dl/index.html')
